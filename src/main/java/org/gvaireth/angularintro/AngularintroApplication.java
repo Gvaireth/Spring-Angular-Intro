562833b3ea1236a934ebb1a420ddc88ef0450d28
gvaireth <pzajaczkowski@gmail.com>
2016-08-25 15:39:20 +0200

    application skeleton from spring initializr

---
+++ src/main/java/org/gvaireth/angularintro/AngularintroApplication.java
@@ -0,0 +1,12 @@
+package org.gvaireth.angularintro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularintroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularintroApplication.class, args);
+	}
+}
---
+++ src/test/java/org/gvaireth/angularintro/AngularintroApplicationTests.java
@@ -0,0 +1,16 @@
+package org.gvaireth.angularintro;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AngularintroApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
