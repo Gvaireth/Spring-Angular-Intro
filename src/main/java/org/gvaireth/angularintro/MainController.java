790c9a39ca3c9535be2ec931381386c03c2c2e07
gvaireth <pzajaczkowski@gmail.com>
2016-08-26 13:05:01 +0200

    deployment assembly restructured, htlm view working

--- src/main/java/org/gvaireth/angularintro/MainController.java
+++ src/main/java/org/gvaireth/angularintro/MainController.java
@@ -7,11 +7,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
 
-	@RequestMapping("/start")
-	public String helloWorld2(Model model) {
-		model.addAttribute("status", "ok");
-		System.out.println("front controller up");
-		return "/index.html";
-	}
+	// @RequestMapping("/start")
+	// public String helloWorld2(Model model) {
+	// model.addAttribute("status", "ok");
+	// System.out.println("front controller up");
+	// return "/index.html";
+	// }
 
 }
