efc846ed0549972e4df5ec5fd357181b15c59504
Gvaireth <pzajaczkowski@gmail.com>
2017-03-12 13:12:37 +0100

    db save

--- src/main/java/org/gvaireth/springangularintro/database/Book.java
+++ src/main/java/org/gvaireth/springangularintro/database/Book.java
@@ -17,7 +17,7 @@ public class Book {
 
 	@Id
 	@GeneratedValue
-	private long id;
+	private Long id;
 
 	@Column
 	private String name;
--- src/main/java/org/gvaireth/springangularintro/web/DatabaseController.java
+++ src/main/java/org/gvaireth/springangularintro/web/DatabaseController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.gvaireth.springangularintro.database.Book;
 import org.gvaireth.springangularintro.database.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,8 +17,16 @@ public class DatabaseController {
 	@Autowired
 	BookRepository bookRepository;
 
-	@RequestMapping("/list")
-	public List<Book> listBooks() {
+	@RequestMapping("/book/findall")
+	public List<Book> findAll() {
 		return bookRepository.findAll();
+	}
+
+	@RequestMapping(value = "/book/add", method = RequestMethod.PUT)
+	public String addBook(@RequestBody Book book) {
+		System.out.println(book);
+		book.setId(null);
+		bookRepository.save(book);
+		return "We now have " + bookRepository.count() + " books!";
 	}
 }
