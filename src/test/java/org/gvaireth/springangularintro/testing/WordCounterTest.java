f4b6412ad790fa9c6db5bd3249327b4a023bc2c1
Gvaireth <pzajaczkowski@gmail.com>
2017-03-19 14:47:46 +0100

    more matchers

--- src/test/java/org/gvaireth/springangularintro/testing/WordCounterTest.java
+++ src/test/java/org/gvaireth/springangularintro/testing/WordCounterTest.java
@@ -1,8 +1,14 @@
 package org.gvaireth.springangularintro.testing;
 
+import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
+import java.util.List;
+
+import org.hamcrest.Matchers;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -14,33 +20,43 @@ import com.tngtech.java.junit.dataprovider.DataProviderRunner;
 @RunWith(DataProviderRunner.class)
 public class WordCounterTest {
 
-	private WordCounter wordCouter;
+	private WordCounter wordCounter;
 
 	@Test
 	public void testWordCounting() {
-		assertThat(wordCouter.countWords("Hello world!"), equalTo(2));
+		assertEquals(wordCounter.countWords("Hello world!"), 2);
+		assertThat(wordCounter.countWords("Hello world!"), equalTo(2));
+	}
+
+	@Test
+	public void testLetterCounting() {
+		List<Integer> result = wordCounter.countLetters("Counting letters in words");
+		assertThat(result, hasSize(4));
+		assertThat(result, contains(8, 7, 2, 5));
+		assertThat(result, Matchers.hasItem(7));
+		assertThat(result.get(0), equalTo(8));
 	}
 
 	@Test
 	@DataProvider({ "2,Hello world!", "1,hi" })
-	public void testLetterCounting(int words, String sentence) {
-		assertThat(wordCouter.countWords(sentence), equalTo(words));
+	public void testWordCountingWithData(int words, String sentence) {
+		assertThat(wordCounter.countWords(sentence), equalTo(words));
 	}
 
 	@Test
 	@DataProvider(value = { "3; one, two, three" }, splitBy = ";")
 	public void testLetterCountingWithComas(int words, String sentence) {
-		assertThat(wordCouter.countWords(sentence), equalTo(words));
+		assertThat(wordCounter.countWords(sentence), equalTo(words));
 	}
 
 	@Before
 	public void setup() {
-		wordCouter = new WordCounter();
+		wordCounter = new WordCounter();
 	}
 
 	@After
 	public void teardown() {
-		wordCouter = new WordCounter();
+		wordCounter = new WordCounter();
 	}
 
 }
