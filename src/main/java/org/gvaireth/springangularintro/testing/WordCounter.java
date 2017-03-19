7f22ffa7f4908eda6ae4bf4327f50beb36bbf1f1
Gvaireth <pzajaczkowski@gmail.com>
2017-03-19 11:36:56 +0100

    DataProvider

--- src/main/java/org/gvaireth/springangularintro/testing/WordCounter.java
+++ src/main/java/org/gvaireth/springangularintro/testing/WordCounter.java
@@ -1,12 +1,26 @@
 package org.gvaireth.springangularintro.testing;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class WordCounter {
 
 	public int countWords(String sentence) {
-		return sentence.split(" ").length;
+		return sentence != null ? sentence.split(" ").length : 0;
+	}
+
+	public List<Integer> countLetters(String sentence) {
+		List<Integer> result = new ArrayList<>();
+		if (sentence != null) {
+			String[] words = sentence.split(" ");
+			for (String word : words) {
+				result.add(word.length());
+			}
+		}
+		return result;
 	}
 
 }
--- src/test/java/org/gvaireth/springangularintro/testing/WordCounterTest.java
+++ src/test/java/org/gvaireth/springangularintro/testing/WordCounterTest.java
@@ -1,18 +1,37 @@
 package org.gvaireth.springangularintro.testing;
 
-import org.hamcrest.Matchers;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import com.tngtech.java.junit.dataprovider.DataProvider;
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+
+@RunWith(DataProviderRunner.class)
 public class WordCounterTest {
 
 	private WordCounter wordCouter;
 
 	@Test
 	public void testWordCounting() {
-		Assert.assertThat(wordCouter.countWords("Hello world!"), Matchers.equalTo(2));
+		assertThat(wordCouter.countWords("Hello world!"), equalTo(2));
+		assertThat(wordCouter.countWords(null), equalTo(0));
+	}
+
+	@Test
+	@DataProvider({ "2,Hello world!", "1,hi" })
+	public void testLetterCounting(int words, String sentence) {
+		assertThat(wordCouter.countWords(sentence), equalTo(words));
+	}
+
+	@Test
+	@DataProvider(value = { "3; one, two, three" }, splitBy = ";")
+	public void testLetterCountingWithComas(int words, String sentence) {
+		assertThat(wordCouter.countWords(sentence), equalTo(words));
 	}
 
 	@Before
